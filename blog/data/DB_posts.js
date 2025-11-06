03206c914f94baea6c086aa2c82566359ab8ea4c
borgoo <borgo-ale@libero.it>
2025-11-06 19:05:54 +0100

    feat(blog): add first part of the new post "Interviews Q&A" with detailed questions and answers, and update sitemap

--- blog/data/DB_posts.js
+++ blog/data/DB_posts.js
@@ -9,6 +9,16 @@
     "authorNickname": "@boborgo",
     "createdDate": "2025-10-09",
     "updatedDate": "2025-10-09"
+  },
+  {
+    "id": "interviews-qa",
+    "contentFile": "posts/interviews-qa.html",
+    "title": "Interviews Q&A",
+    "subtitle": "A pragmatic list of questions that I've encountered during my job search",
+    "abstract": "A compilation of all the questions (theoretical and practical) that I encountered during my job search, grouped by company and position.",    "author": "Alessandro Borgonovo",
+    "authorNickname": "@boborgo",
+    "createdDate": "2025-11-06",
+    "updatedDate": "2025-11-06"
   }
 ]
 
